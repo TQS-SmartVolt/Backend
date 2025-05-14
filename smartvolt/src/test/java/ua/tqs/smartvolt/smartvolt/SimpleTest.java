24d418088f195ec16257616ba31079a8c158c0d4
Tomás Santos <t.santos.f@ua.pt>
2025-05-14 22:08:21 +0100

    xray

---
+++ smartvolt/src/test/java/ua/tqs/smartvolt/smartvolt/SimpleTest.java
@@ -0,0 +1,14 @@
+package ua.tqs.smartvolt.smartvolt;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class SimpleTest {
+
+  @Test
+  void testAddition() {
+    System.out.println("Running SimpleTest.testAddition()");
+    assertEquals(2, 1 + 1);
+  }
+}
