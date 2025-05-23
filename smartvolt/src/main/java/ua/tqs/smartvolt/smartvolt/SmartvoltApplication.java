5acddb30d1f6e8ce69b06c1dfd2e2048340d1554
Tomás Santos <t.santos.f@ua.pt>
2025-05-23 20:21:48 +0100

    bugfix

--- smartvolt/src/main/java/ua/tqs/smartvolt/smartvolt/SmartvoltApplication.java
+++ smartvolt/src/main/java/ua/tqs/smartvolt/smartvolt/SmartvoltApplication.java
@@ -1,25 +1,12 @@
 package ua.tqs.smartvolt.smartvolt;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SmartvoltApplication {
 
-  private static final Logger logger = LogManager.getLogger(SmartvoltApplication.class);
-
   public static void main(String[] args) {
     SpringApplication.run(SmartvoltApplication.class, args);
-    // Wait 1 min and test 100x logs
-    try {
-      Thread.sleep(60000);
-    } catch (InterruptedException e) {
-      logger.error("Error while waiting", e);
-    }
-    for (int i = 0; i < 10; i++) {
-      logger.info("TestLogNumber" + i);
-    }
   }
 }
