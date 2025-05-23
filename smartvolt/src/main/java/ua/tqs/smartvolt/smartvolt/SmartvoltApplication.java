99352d32c6daaefd5e32ee9b7cab3ef6e8dc3c5e
Tomás Santos <t.santos.f@ua.pt>
2025-05-23 16:40:53 +0100

    hotfix

--- smartvolt/src/main/java/ua/tqs/smartvolt/smartvolt/SmartvoltApplication.java
+++ smartvolt/src/main/java/ua/tqs/smartvolt/smartvolt/SmartvoltApplication.java
@@ -1,12 +1,20 @@
 package ua.tqs.smartvolt.smartvolt;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SmartvoltApplication {
 
+  private static final Logger logger = LogManager.getLogger(SmartvoltApplication.class);
+
   public static void main(String[] args) {
     SpringApplication.run(SmartvoltApplication.class, args);
+    // Test 100x logs
+    for (int i = 0; i < 100; i++) {
+      logger.info("Test log number: " + i);
+    }
   }
 }
