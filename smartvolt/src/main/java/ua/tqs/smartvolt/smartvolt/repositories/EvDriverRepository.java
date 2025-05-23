f12a43c04ba1d97630bc696566cf67c05a13bd0d
Joao Pinto <jpapinto@ua.pt>
2025-05-23 11:46:08 +0100

    working

--- smartvolt/src/main/java/ua/tqs/smartvolt/smartvolt/repositories/EvDriverRepository.java
+++ smartvolt/src/main/java/ua/tqs/smartvolt/smartvolt/repositories/EvDriverRepository.java
@@ -6,8 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ua.tqs.smartvolt.smartvolt.models.EvDriver;
 
-import java.util.Optional;
-
 @Repository
 public interface EvDriverRepository extends JpaRepository<EvDriver, Long> {
   // Custom query methods can be defined here if needed
