694431fc9dfb99cb5a079396d7601dc7c80f6854
Pedro Pinto <pmap@ua.pt>
2025-05-24 12:58:00 +0100

    fixing pom.xml for multiple profiles

--- smartvolt/src/test/java/ua/tqs/smartvolt/smartvolt/functional_tests/CucumberTest.java
+++ smartvolt/src/test/java/ua/tqs/smartvolt/smartvolt/functional_tests/CucumberTest.java
@@ -1,6 +1,5 @@
 package ua.tqs.smartvolt.smartvolt.functional_tests;
 
-import static io.cucumber.junit.platform.engine.Constants.FILTER_TAGS_PROPERTY_NAME;
 import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 
 import org.junit.platform.suite.api.ConfigurationParameter;
@@ -12,5 +11,5 @@ import org.junit.platform.suite.api.Suite;
 @IncludeEngines("cucumber")
 @SelectClasspathResource("ua/tqs/smartvolt/smartvolt")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "ua.tqs.smartvolt.smartvolt")
-@ConfigurationParameter(key = FILTER_TAGS_PROPERTY_NAME, value = "@UAT-Web")
+@ConfigurationParameter(key = "cucumber.filter.tags", value = "@UAT-Web")
 public class CucumberTest {}
