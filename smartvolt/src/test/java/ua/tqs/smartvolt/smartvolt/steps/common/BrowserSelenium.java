f8e2c399e68d9e86577fbb4674fc47cec9e865df
Pedro Pinto <pmap@ua.pt>
2025-06-02 11:42:43 +0100

    fix workflows

--- smartvolt/src/test/java/ua/tqs/smartvolt/smartvolt/steps/common/BrowserSelenium.java
+++ smartvolt/src/test/java/ua/tqs/smartvolt/smartvolt/steps/common/BrowserSelenium.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.remote.AbstractDriverOptions;
 import org.testcontainers.containers.BrowserWebDriverContainer;
+import org.testcontainers.utility.DockerImageName;
 import ua.tqs.smartvolt.smartvolt.MyTestConfiguration;
 
 public class BrowserSelenium {
@@ -47,7 +48,9 @@ public class BrowserSelenium {
   @SuppressWarnings("resource")
   private static BrowserSelenium getFirefox() {
     return new BrowserSelenium(
-        new BrowserWebDriverContainer<>().withCapabilities(new FirefoxOptions()),
+        new BrowserWebDriverContainer<>(
+                DockerImageName.parse("selenium/standalone-firefox:4.33.0-20250525"))
+            .withCapabilities(new FirefoxOptions()),
         new FirefoxOptions());
   }
 
